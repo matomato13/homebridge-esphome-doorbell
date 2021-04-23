8c74913c17c2da8fa946ba83625c2fcd7875274b
Mathieu Masciotra <matomato13@hotmail.com>
2021-04-23 16:48:54 -0400

    Add mute option

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,6 +1,7 @@
 import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 import { EspHomeDoorbellPlatform, IEsphomePlatformDevice } from './platform';
 import { EspHomeWebApi, EspHomeStateEventData } from './esphome_webapi';
+import fetch from 'node-fetch';
 
 export interface DoorbellAccessoryContext {
   device: IEsphomePlatformDevice;
@@ -15,6 +16,8 @@ export class DoorbellAccessory {
   private service: Service;
   private espHomeWebApi: EspHomeWebApi;
 
+  private isMute: boolean;
+
   constructor(
     private readonly platform: EspHomeDoorbellPlatform,
     private readonly accessory: PlatformAccessory<DoorbellAccessoryContext>,
@@ -28,125 +31,67 @@ export class DoorbellAccessory {
       .setCharacteristic(this.platform.Characteristic.Model, 'ESP8266')
       .setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name!);
 
+    // remove ununsed information
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .removeCharacteristic(new this.platform.Characteristic.SerialNumber());
+
     // get the Doorbell service if it exists, otherwise create a new Doorbell service
     // you can create multiple services for each accessory
     this.service = this.accessory.getService(this.platform.Service.Doorbell) || this.accessory.addService(this.platform.Service.Doorbell);
 
-    // // set the service name, this is what is displayed as the default name on the Home app
-    // // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    // this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
-
     this.espHomeWebApi.stateEvent((event: EspHomeStateEventData) => this.handleEvent(event));
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
-
-    // register handlers for the On/Off Characteristic
-    // this.service.getCharacteristic(this.platform.Characteristic.On)
-    //   .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-    //   .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
-
-    // // register handlers for the Brightness Characteristic
-    // this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-    //   .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
-
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    // const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-    //   this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    // const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-    //   this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    // let motionDetected = false;
-    // setInterval(() => {
-    //   // EXAMPLE - inverse the trigger
-    //   motionDetected = !motionDetected;
-
-    //   // push the new value to HomeKit
-    //   motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-    //   motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-    //   this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-    //   this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    // }, 10000);
+    // register handlers for the Mute Characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.Mute)
+      .onSet(this.setMute.bind(this))
+      .onGet(this.getMute.bind(this));
   }
 
   private handleEvent(event: EspHomeStateEventData) {
-    if (event.id === 'binary_sensor-doorbell_button') {
-      this.platform.log.debug(`[ES][${this.accessory.context.device.name}] State event:`, event);
+    this.platform.log.debug(`[ES][${this.accessory.context.device.name}] State event:`, event);
+
+    if (this.isDoorbellButton(event.id)) {
       if (event.value) {
         this.platform.log.info('Doorbell activated');
         this.service.getCharacteristic(this.platform.Characteristic.ProgrammableSwitchEvent)
           .updateValue(this.platform.Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS);
       }
     }
+
+    if (this.isDoorbellChimeActiveSwitch(event.id)) {
+      const isActive = event.value as boolean;
+      const isMute = !isActive;
+      this.platform.log.info(`Doorbell chime active changed to ${isActive}`);
+      this.service.updateCharacteristic(this.platform.Characteristic.Mute, isMute as CharacteristicValue);
+      this.isMute = isMute;
+    }
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    //this.exampleStates.On = value as boolean;
+  async setMute(value: CharacteristicValue) {
+    this.platform.log.debug('Set Characteristic Mute ->', value);
 
-    this.platform.log.debug('Set Characteristic On ->', value);
+    const isMuting = value as boolean;
+    const requestUrl = isMuting
+      ? '/switch/doorbell_chime_active/turn_off'
+      : '/switch/doorbell_chime_active/turn_on';
+
+    fetch(requestUrl, { method: 'POST' })
+      .then(() => {
+        this.isMute = isMuting;
+      })
+      .catch(err => this.platform.log.error(`Error occurred while changing chime state: ${err}`));
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    //const isOn = this.exampleStates.On;
-
-    this.platform.log.debug('Get Characteristic On ->', true);
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return true;
+  async getMute(): Promise<CharacteristicValue> {
+    this.platform.log.debug('Get Characteristic Mute ->', this.isMute);
+    return this.isMute;
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    //this.exampleStates.Brightness = value as number;
-
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+  private isDoorbellButton(eventId: string): boolean {
+    return eventId === 'binary_sensor-doorbell_button';
   }
 
+  private isDoorbellChimeActiveSwitch(eventId: string): boolean {
+    return eventId === 'switch-doorbell_chime_active';
+  }
 }
